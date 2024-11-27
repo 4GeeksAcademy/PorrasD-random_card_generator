9804f24453a1e5866f841bfd1e49ebe07fe17697
PorrasD <122325175+PorrasD@users.noreply.github.com>
2024-11-27 04:58:50 +0000

    card_generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,50 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+const suits = ["♥", "♦", "♣", "♠"];
+const numbers = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let cardNumber = document.getElementById("card-number");
+let cardSuit = document.getElementById("card-suit");
+let cardSuitBottom = document.getElementById("card-suit-bottom");
+let card = document.getElementById("card");
+let generateCardButton = document.getElementById("generate-card");
+
+function generateRandomCard() {
+  let randomSuitIndex = Math.floor(Math.random() * suits.length);
+  let randomNumberIndex = Math.floor(Math.random() * numbers.length);
+
+  let randomSuit = suits[randomSuitIndex];
+  let randomNumber = numbers[randomNumberIndex];
+
+  cardNumber.textContent = randomNumber;
+  cardSuit.textContent = randomSuit;
+  cardSuitBottom.textContent = randomSuit;
+
+  if (randomSuit === "♥" || randomSuit === "♦") {
+    cardSuit.style.color = "red";
+    cardSuitBottom.style.color = "red";
+  } else {
+    cardSuit.style.color = "black";
+    cardSuitBottom.style.color = "black";
+  }
+}
+
+generateCardButton.addEventListener("click", generateRandomCard);
+window.onload = generateRandomCard;
